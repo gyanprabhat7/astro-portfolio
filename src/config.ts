cc8d8125e776c6b187459bf0f4fe245a161e3898
Gyan Prabhat <gyanprabhat07@gmail.com>
2024-12-17 10:56:53 +0530

    update

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Gyan Prabhat ';
+export const SITE_DESCRIPTION = 'Portfolio & Project';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
